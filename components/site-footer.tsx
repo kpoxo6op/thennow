c9ffdacea5e2203c3e211a9a90432aaa4be551ce
kpoxo6op <kpoxo6op@gmail.com>
2024-08-30 15:28:53 +1200

    replace mobile menu with basic footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,24 +1,29 @@
-import Link from "next/link";
-import { siteConfig } from "@/config/site"
-import { MainNav } from "@/components/main-nav"
 import * as React from "react"
-import { cn } from "@/lib/utils"
+import Link from "next/link"
 
-const SiteFooter = React.forwardRef<HTMLElement, React.HTMLAttributes<HTMLElement>>(
-  ({ className, ...props }, ref) => {
-    return (
-      <footer
-        ref={ref}
-        className={cn("container flex flex-col items-center gap-4 text-left", className)}
-        {...props}
-      >
-        <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
-          <MainNav items={siteConfig.mainNav} />
-        </div>
-      </footer>
-    )
-  }
-)
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { MainNav } from "@/components/main-nav"
+
+const SiteFooter = React.forwardRef<
+  HTMLElement,
+  React.HTMLAttributes<HTMLElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <footer
+      ref={ref}
+      className={cn(
+        "container flex flex-col items-center gap-4 text-left",
+        className
+      )}
+      {...props}
+    >
+      <div className="flex h-16 items-center space-x-4 text-center sm:justify-between sm:space-x-0">
+        {siteConfig.name}
+      </div>
+    </footer>
+  )
+})
 SiteFooter.displayName = "SiteFooter"
 
 export { SiteFooter }
